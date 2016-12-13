2c62697a26c02311a626346188708b7aa405f9ab
dmccullough <dmccullough@decisionpatterns.com>
2016-12-13 15:43:19 -0500

    refactor revealPoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,16 @@
 var animatePoints = function() {
 
     var points = document.getElementsByClassName('point');
-
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-        points[0].style.backgroundColor = "blue";
-        var title = points[0].getElementsByTagName("h5");
-        title = title[0];
-        title.style.fontSize = "60px";
-    };
-
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    
+    var revealPoint = function(i) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+    
+    for (var i=0; i < points.length; i++) {
+        revealPoint(i);
+    }
 
 };
