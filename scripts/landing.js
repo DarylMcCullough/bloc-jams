ec86a014b257e2fe74382c4ce768abdb34d50841
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-01-02 18:48:52 -0500

    remove obsolete code

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,3 @@
-var pointsArray = document.getElementsByClassName('point');
-
 var animatePoints = function() {
     var revealPoint = function() {
         $(this).css({
@@ -15,8 +13,6 @@ $(window).load(function() {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    //var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
      $(window).scroll(function(event) {
